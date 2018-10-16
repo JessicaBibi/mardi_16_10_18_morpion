d36544e6f52122c346e8f94bba369a5bc5672ce8
Malo De Kervasdoué <43620886+malo2k@users.noreply.github.com>
2018-10-16 22:50:45 +0200

    Update Boardcase.rb

--- Boardcase.rb
+++ Boardcase.rb
@@ -1,17 +1,16 @@
 class BoardCase
   #TO DO : la classe a 2 attr_accessor, sa valeur (X, O, ou vide), ainsi que son numéro de case)
-  attr_accessor :value, :number_case
-
+  attr_accessor :valeur, :case_number
   
-  def initialize
+  def initialize (valeur, case_number)
     #TO DO doit régler sa valeur, ainsi que son numéro de case
-    @value = value
-    @number_case = number_case
+    @valeur = valeur
+    @case_number = case_number
   end
   
   def to_s
     #TO DO : doit renvoyer la valeur au format string
-    return "#{value}"
+    return @valeur.to_s 
   end
 
 end
