36047649cdb6d2a4b73df98ec178fdfe0de8230a
Malo De Kervasdoué <43620886+malo2k@users.noreply.github.com>
2018-10-16 22:51:46 +0200

    Update Player.rb

--- Player.rb
+++ Player.rb
@@ -1,12 +1,20 @@
 class Player
   #TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
-  attr_accessor :player_name, :player_vlaue
-  attr_writer :status
-  attr_reader :
-
+attr_accessor :nom_player, :valeur_player
+attr_writer :status
+attr_reader :list_case_p
   
-  def initialize(player_name)
+  def initialize (nom_player, valeur_player)
     #TO DO : doit régler son nom, sa valeur, son état de victoire
-    @player_name = player_name
+      @nom_player = nom_player
+      @valeur_player = valeur_player
+      @status = "il a gagné ?"
 
+      @list_case_p = Array.new
   end
+
+  def list_case(case_num)  #List the cases that belongs to the player
+      
+      @list_case_p.push(case_num)
+  end 
+end 
